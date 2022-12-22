452b138f3814f56d78fae19fc968a5f0071355a4
AppleSoju <patryknmarciniak@gmail.com>
2022-12-22 12:10:48 +0100

    added derivative functions for relu and sigmoid, also removed linear function

--- layers/activ_funs.py
+++ layers/activ_funs.py
@@ -1,16 +1,20 @@
 import numpy as np
 
-
-def linear(x):
-    return x
-
-
 def relu(x):
     out = max(0, x)
     return out
 
+def relu_prime():
+    out = (x > 0) * 1
+    return out
 
 def sigmoid(x):
     ex = np.e ** x
     out = ex / (ex + 1)
+    return out
+
+def sigmoid_prime(x):
+    ex = np.e ** x
+    f = ex / (ex + 1)
+    out = f * (1 - f)
     return out
