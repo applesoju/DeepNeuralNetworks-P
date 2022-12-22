f1746dd29269e6d727fa4e2121f630084720263a
AppleSoju <patryknmarciniak@gmail.com>
2022-12-22 09:29:19 +0100

    changed implementation of ConvolutionalLayer and PoolingLayer classes so the parameters can be set with constructor

--- conv_layer.py
+++ conv_layer.py
@@ -4,10 +4,12 @@ from layer import Layer
 
 
 class ConvolutionalLayer(Layer):
-    def __init__(self):
+    def __init__(self, kernel, strides=1):
         super().__init__()
-        self.filter = None
-        self.padding = None
+
+        self.kernel = kernel
+        self.padding = kernel.shape[0] // 2
+        self.strides = strides
 
     def compute_output(self, input_data):
         super().compute_output(input_data)
@@ -15,19 +17,12 @@ class ConvolutionalLayer(Layer):
         self.outputs = self.perform_convolution()
         return self.outputs
 
-    def set_filters(self, filter_obj):  # TODO: more than one filter
-        self.filter = filter_obj
-        self.padding = filter_obj.shape[0] // 2
-
-    def perform_convolution(self, strides=1):
+    def perform_convolution(self):
         input_shape = (self.inputs.shape[0], self.inputs.shape[1])
         padd = self.padding
+        strides = self.strides
 
-        if self.filter is None:
-            print('Layer is missing a filter')
-            return
-
-        kernel = np.flipud(np.fliplr(self.filter))
+        kernel = np.flipud(np.fliplr(self.kernel))
         output_shape = (
             int(((input_shape[0] - kernel.shape[0] + 2 * padd) / strides) + 1),
             int(((input_shape[1] - kernel.shape[1] + 2 * padd) / strides) + 1)
@@ -45,16 +40,10 @@ class ConvolutionalLayer(Layer):
             output_padded[int(padd): int(padd * -1), int(padd): int(padd * -1)] = self.inputs
 
         for row in range(input_shape[1]):
-            # if row > input_shape[1] - kernel.shape[1]:
-            #     break
-
             if row % strides != 0:
                 continue
 
             for col in range(input_shape[0]):
-                # if col > input_shape[0] - kernel.shape[0]:
-                #     break
-
                 if col % strides == 0:
                     output[col, row] = (
                             kernel * output_padded[col: col + kernel.shape[0], row: row + kernel.shape[1]]
--- pool_layer.py
+++ pool_layer.py
@@ -4,22 +4,20 @@ from layer import Layer
 
 
 class PoolingLayer(Layer):
-    def __init__(self):
+    def __init__(self, kernel_shape, n_strides, method='max'):
         super().__init__()
-        self.kernel_shape = None
-        self.n_strides = None
 
-    def compute_output(self, input_data):
-        super().compute_output(input_data)
+        self.kernel_shape = kernel_shape
+        self.n_strides = n_strides
+        self.method = method
+
+    def compute_output(self, inputs):
+        super().compute_output(inputs)
 
         self.outputs = self.perform_pooling()
         return self.outputs
 
-    def set_kernel_and_strides(self, kernel_shape, n_strides):
-        self.kernel_shape = kernel_shape
-        self.n_strides = n_strides
-
-    def perform_pooling(self, method='max'):
+    def perform_pooling(self):
         input_shape = (self.inputs.shape[0], self.inputs.shape[1])
 
         output_shape = (
@@ -39,7 +37,7 @@ class PoolingLayer(Layer):
 
                 pool_slice = self.inputs[x_start: x_end, y_start: y_end]
 
-                match method:
+                match self.method:
                     case 'max':
                         output[x, y] = np.max(pool_slice)
                     case 'avg':
