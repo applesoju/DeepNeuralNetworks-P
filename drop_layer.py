adef8340a7b4cf6dde784d1981213dfe233e11be
AppleSoju <patryknmarciniak@gmail.com>
2022-12-22 09:39:05 +0100

    changed implementation of ActivationLayer and DropoutLayer classes so the parameters can be set with constructor

--- drop_layer.py
+++ drop_layer.py
@@ -4,9 +4,10 @@ from layer import Layer
 
 
 class DropoutLayer(Layer):
-    def __init__(self):
+    def __init__(self, probability):
         super().__init__()
-        self.propability = None
+
+        self.probability = probability
 
     def compute_output(self, input_data):
         super().compute_output(input_data)
@@ -14,14 +15,11 @@ class DropoutLayer(Layer):
         self.outputs = self.perform_dropout()
         return self.outputs
 
-    def set_propability(self, prop):
-        self.propability = prop
-
     def perform_dropout(self):
         output = np.zeros(self.inputs.shape)
 
         for i, _ in enumerate(output):
-            if np.random.rand() < 1 - self.propability:
+            if np.random.rand() < 1 - self.probability:
                 output[i] = self.inputs[i]
 
         return output
