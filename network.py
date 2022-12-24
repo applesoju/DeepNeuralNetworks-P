3fef7e5860cc4703a0188e4a115120465411f7c9
AppleSoju <patryknmarciniak@gmail.com>
2022-12-24 13:55:40 +0100

    preparation for implementing backpropagation

--- network.py
+++ network.py
@@ -1,20 +1,30 @@
 class Network:
-    def __init__(self):
+    def __init__(self, loss_fun, deriv_loss_fun):
         self.layers = []
-        self.loss = None
+        self.loss = loss_fun
+        self.loss_prime = deriv_loss_fun
 
     def add(self, layer):
         self.layers.append(layer)
 
-    def train(self, input_layer):
+    def train(self, input_layer, correct_output, epochs=None, learn_rate=None):
         input_layer = input_layer.astype('float64')
-        next_layer = input_layer / 255.0
+        output = input_layer / 255.0
 
         for layer in self.layers:
-            print(next_layer.shape)
-            next_layer = layer.compute_output(next_layer)
+            print(output.shape)
+            output = layer.compute_output(output)
 
-        return next_layer
+        print(output)
+
+        output_error = self.loss_prime(correct_output, output)
+
+        # for layer in self.layers[::-1]:
+        #     print(output.shape)
+        #     output = layer.perform_backward_prop()
+
+
+        return output
 
     def classify(self, input_layer):
         raise NotImplementedError
