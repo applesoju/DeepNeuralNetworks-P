cc16fd4c5563101670c56f6b4fac9ae54657f921
AppleSoju <patryknmarciniak@gmail.com>
2022-12-24 14:45:08 +0100

    exchanged sigmoid function for softmax and changed implementation of relu so it accepts numpy arrays

--- layers/funs.py
+++ layers/funs.py
@@ -1,23 +1,17 @@
 import numpy as np
 
 def relu(x):
-    out = max(0, x)
-    return out
+    return np.maximum(0, x)
 
 def relu_prime(x):
-    out = (x > 0) * 1
-    return out
+    return (x > 0) * 1
 
-def sigmoid(x):
-    ex = np.e ** x
-    out = ex / (ex + 1)
-    return out
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
 
-def sigmoid_prime(x):
-    ex = np.e ** x
-    f = ex / (ex + 1)
-    out = f * (1 - f)
-    return out
+def softmax_prime(softmax_output):
+    s = softmax_output.reshape(-1, 1)
+    return np.diagflat(s) - np.dot(s, s.T)
 
 def mse(correct_val, predicted_val):
     return np.mean(np.power(correct_val - predicted_val, 2))
