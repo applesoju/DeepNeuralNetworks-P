fixed backpropagation implementation for DenseLayer

layers/dens_layer.py
@@ -13,9 +13,6 @@ class DenseLayer(Layer):
         self.weights = np.random.rand(input_size, output_size) - 0.5
         self.bias = np.random.rand(1, output_size) - 0.5
 
-    def set_input_size(self, input_size):
-        self.input_size = input_size
-
     def compute_output(self, input_data):
         super().compute_output(input_data)
 
@@ -27,10 +24,14 @@ class DenseLayer(Layer):
         return output
 
     def perform_backward_prop(self, output_err, learn_rate):
+        print(self.weights.shape)
         input_err = np.dot(output_err, self.weights.T)
-        weights_err = np.dot(self.inputs.T, output_err)
+        weights_err = np.dot(
+            self.inputs.reshape(self.input_size, 1),
+            output_err.reshape(1, self.output_size)
+        )
         bias_err = output_err
-
+        print(weights_err.shape)
         self.weights -= weights_err * learn_rate
         self.bias -= bias_err * learn_rate
 