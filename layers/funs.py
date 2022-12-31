76771e946ba918b463523e470d90e34f5b09a2ec
AppleSoju <patryknmarciniak@gmail.com>
2022-12-31 01:22:43 +0100

    implemented sigmoid function again

--- layers/funs.py
+++ layers/funs.py
@@ -16,26 +16,21 @@ def softmax_prime(softmax_output):
     s = softmax_output.reshape(-1, 1)
     return np.diag(s) - s * s.T
 
+def sigmoid(x):
+    ex = np.e ** x
+    out = ex / (ex + 1)
+    return out
+
+def sigmoid_prime(x):
+    ex = np.e ** x
+    f = ex / (ex + 1)
+    out = f * (1 - f)
+    return out
+
+
 def mse(correct_val, predicted_val):
     return np.mean(np.power(correct_val - predicted_val, 2))
 
 def mse_prime(correct_val, predicted_val):
     return 2 * (predicted_val - correct_val) / correct_val.size
 
-def cross_entropy(x, y):
-    m = y.shape[0]
-    p = stable_softmax(x)
-
-    log_likehood = -np.log(p[range(m), y])
-    loss = np.sum(log_likehood) / m
-
-    return loss
-
-def cross_entropy_prime(x, y):
-    m = y.shape[0]
-
-    grad = stable_softmax(x)
-    grad[range(m), y] -= 1
-    grad /= m
-
-    return grad
