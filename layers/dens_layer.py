8cc8a95f725936eb40c96996a7a1c04be3654653
AppleSoju <patryknmarciniak@gmail.com>
2023-01-04 01:08:42 +0100

    implemented forward propagation with activation for DenseLayer

--- layers/dens_layer.py
+++ layers/dens_layer.py
@@ -2,19 +2,24 @@ import numpy as np
 
 
 class DenseLayer:
-    def __init__(self, input_shape, n_neurons, activation, activation_prime, weights=None, bias=None):
-        self.input_shape = input_shape
+    def __init__(self, input_size, n_neurons, activation, activation_prime, weights=None, bias=None):
+        self.input_size = input_size
         self.input = None
 
         self.n_neurons = n_neurons
+        self.output = None
+
         self.activation = activation
         self.activation_prime = activation_prime
 
         self.weights = np.random.rand(input_size, n_neurons) - 0.5 if weights is not None else weights
-        self.bias = np.random.rand(1, n_neurons) - 0.5 if bias is not None else bias
+        self.biases = np.random.rand(1, n_neurons) - 0.5 if bias is not None else bias
 
-    def forward_prop(self):
-        raise NotImplementedError
+    def forward_prop(self, layer_input):
+        dense_output = np.dot(layer_input, self.weights) + self.biases
+        activated_output = self.activation(dense_output)
 
-    def backward_prop(self):
+        return activated_output
+
+    def backward_prop(self, next_layer):
         raise NotImplementedError
