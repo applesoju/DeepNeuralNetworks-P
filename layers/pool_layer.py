fixed backpropagation for MaxPoolingLayer

layers/pool_layer.py
@@ -17,7 +17,7 @@ class MaxPoolingLayer:
         self.output = np.zeros(self.output_shape)
 
         # Prepare delta variable for backpropagation
-        self.delta = None
+        self.delta = np.zeros(self.input_shape)
 
     def forward_prop(self, layer_input):
         self.input = layer_input
@@ -46,14 +46,23 @@ class MaxPoolingLayer:
         return self.output
 
     def backward_prop(self, next_layer):    # TODO: check and maybe switch for the same loop as forward_prop()
+        row_range = range(0, self.input_shape[0], self.kernel_shape[0])
+        col_range = range(0, self.input_shape[1], self.kernel_shape[1])
+
         # For each filter in layer
         for f in range(self.output_shape[2]):
             # For each row
-            for r in range(self.output_shape[0]):
+            for r in row_range:
+                r_end = r + self.kernel_shape[0]
+
                 # For each column
-                for c in range(self.output_shape[1]):
+                for c in col_range:
+                    c_end = c + self.kernel_shape[1]
+
                     # Get delta from downstream
-                    delta_output = next_layer.delta[r, c, f]
+                    delta_output = next_layer.delta[r // self.kernel_shape[0],
+                                                    c // self.kernel_shape[1],
+                                                    f]
 
                     # Determine input array indices and get its chunk
                     input_row_start = r * self.kernel_shape[0]
@@ -62,14 +71,13 @@ class MaxPoolingLayer:
                     input_col_start = c * self.kernel_shape[1]
                     input_col_end = input_col_start + self.kernel_shape[1]
 
-                    chunk = self.input[input_row_start: input_row_end,
-                                       input_col_start: input_col_end,
-                                       f]
+                    chunk = self.input[r: r_end, c: c_end, f]
                     max_value = np.max(chunk)
-                    max_value_index = np.argwhere(chunk == max_value)[0]
+                    max_value_indices = np.argwhere(chunk == max_value)
 
-                    self.delta[r + max_value_index[0],
-                               c + max_value_index[1],
-                               f] = delta_output
+                    for indices in max_value_indices:
+                        self.delta[r + indices[0],
+                                   c + indices[1],
+                                   f] = delta_output
 
 