removed unecessary variable, added output reshaping and fixed error computation

layers/flat_layer.py
@@ -6,8 +6,7 @@ class FlatteningLayer:
         self.input = None
         self.input_shape = input_shape
 
-        # Layer output and its size
-        self.output_size = np.prod(self.input_shape)
+        # Layer output
         self.output = None
 
         # Prepare error and delta variables for backpropagation
@@ -16,12 +15,12 @@ class FlatteningLayer:
 
     def forward_prop(self, layer_input):
         self.input = layer_input
-        self.output = self.input.flatten()
+        self.output = self.input.flatten().reshape(1, -1)
 
         return self.output
 
     def backward_prop(self, next_layer):
         # Compute error from downstream and determine this layers delta term
-        self.error = np.dot(next_layer.weights, next_layer.delta)
+        self.error = np.dot(next_layer.weights, next_layer.delta.T).T
         self.delta = self.error * self.output
         self.delta = self.delta.reshape(self.input_shape)